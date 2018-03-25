6b8fd61e047f235f9d69f783a8d9726766470026
KoutarouYabe <idolm@ster.pw>
2018-03-25 22:55:25 +0900

    add alias: fig -> docker-compose

--- zshcustom/mycustom.zsh
+++ zshcustom/mycustom.zsh
@@ -3,3 +3,5 @@ alias man='env LANG=ja_JP.UTF-8 /usr/local/bin/jman'
 alias jr="jq '.' -C"
 function gi() { curl -L -s https://www.gitignore.io/api/$@ ;}
 alias tf="tail -f"
+alias fig='docker-compose'
+
