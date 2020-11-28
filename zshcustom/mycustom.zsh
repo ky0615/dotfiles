66b6e9a4e8b0aeb75fc737e50d234a3fbc88619c
Kotaro Yabe <idolm@ster.pw>
2020-11-29 04:41:40 +0900

    nodenv向けdefault packageファイルを追加

--- zshcustom/mycustom.zsh
+++ zshcustom/mycustom.zsh
@@ -4,3 +4,6 @@ function gi() { curl -L -s https://www.gitignore.io/api/$@ ;}
 alias tf="tail -f"
 alias fig='docker-compose'
 alias res='sed -e "s/ /\\\ /g"'
+
+alias nodenv-update='nodenv update-version-defs'
+alias nodenv-node-path='nodenv which node'
