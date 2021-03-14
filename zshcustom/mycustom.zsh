eb52dffb89aaad91d1396d96008ee00504a633c3
Kotaro Yabe <idolm@ster.pw>
2021-03-15 03:38:42 +0900

    res commandを修正

--- zshcustom/mycustom.zsh
+++ zshcustom/mycustom.zsh
@@ -3,7 +3,7 @@ alias jr="jq '.' -C"
 function gi() { curl -L -s https://www.gitignore.io/api/$@ ;}
 alias tf="tail -f"
 alias fig='docker-compose'
-alias res='sed -e "s/ /\\\ /g"'
+alias res='sed -e "s/\([][ '\'']\)/ /g"'
 
 alias nodenv-update='nodenv update-version-defs'
 alias nodenv-node-path='nodenv which node'
