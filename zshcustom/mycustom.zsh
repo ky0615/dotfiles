f12feb153d78a802cf60c3cd948ca3c5275bc0e2
Kotaro Yabe <idolm@ster.pw>
2021-03-31 18:37:12 +0900

    update custom command

--- zshcustom/mycustom.zsh
+++ zshcustom/mycustom.zsh
@@ -3,7 +3,8 @@ alias jr="jq '.' -C"
 function gi() { curl -L -s https://www.gitignore.io/api/$@ ;}
 alias tf="tail -f"
 alias fig='docker-compose'
-alias res='sed -e "s/\([][ '\'']\)/ /g"'
+alias res='sed -e "s/\([][ '\'']\)/\\\\\1/g"'
+alias filename="sed -E 's/^([\/]?.*\/)*(.*)\..*$/\2/'"
 
 alias nodenv-update='nodenv update-version-defs'
 alias nodenv-node-path='nodenv which node'
