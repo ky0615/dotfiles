ba4aad8afc6f2ce40b69438d6b599d8b2e57ff05
Kotaro Yabe <tarosa@notespace.jp>
2025-02-02 19:57:12 +0900

    update docker compose command

--- zshcustom/mycustom.zsh
+++ zshcustom/mycustom.zsh
@@ -2,7 +2,7 @@ alias adbd="adb devices"
 alias jr="jq '.' -C"
 function gi() { curl -L -s https://www.gitignore.io/api/$@ ;}
 alias tf="tail -f"
-alias fig='docker-compose'
+alias fig='docker compose'
 alias res='sed -e "s/\([][ '\'']\)/\\\\\1/g"'
 alias filename="sed -E 's/^([\/]?.*\/)*(.*)\..*$/\2/'"
 
